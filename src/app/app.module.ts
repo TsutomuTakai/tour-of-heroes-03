4b104090611c8642144800f32a49f9de472bb8e0
TsutomuTakai <wesley.takai@gmail.com>
2018-05-10 11:26:40 -0300

    again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -9,22 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
-=======
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
     BrowserModule,
     FormsModule
->>>>>>> 423d2b714fc5ad9eb9e659e17680c41f3ab8b1a9
   ],
   providers: [],
   bootstrap: [AppComponent]
